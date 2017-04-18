df2db74deded09d759f71c0695654d8624b563a9
ggaier <jwenbo52@gmail.com>
2017-04-19 00:30:07 +0800

    add oneshotanimation

--- core/src/com/ggaier/gigagalk/animation/Animations.kt
+++ core/src/com/ggaier/gigagalk/animation/Animations.kt
@@ -1,10 +1,15 @@
 package com.ggaier.gigagalk.animation
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.Array
 import com.badlogic.gdx.utils.DelayedRemovalArray
 import com.badlogic.gdx.utils.TimeUtils
 import com.badlogic.gdx.utils.viewport.ExtendViewport
@@ -22,28 +27,77 @@ class Animations : ApplicationAdapter() {
     lateinit var mBatch: SpriteBatch
     lateinit var mViewport: ExtendViewport
 
-    lateinit var mWalkLoop: Animation<Any>
+    lateinit var mWalkLoop: Animation<TextureRegion>
     var mStartTime = Long.MIN_VALUE
 
-    lateinit var mExplosion:Animation<Any>
+    lateinit var mExplosion: Animation<TextureRegion>
     lateinit var mExplosions: DelayedRemovalArray<OneShotAnimation>
 
     override fun create() {
         mBatch = SpriteBatch()
         mViewport = ExtendViewport(100f, 100f)
         mStartTime = TimeUtils.nanoTime()
-        val walkLoopTextures:Array<TextureRegion> = Array(3, { i ->
-            TextureRegion(Texture("walk-$i-right.png")
-            )
-        })
-        mWalkLoop = Animation(WALK_LOOP_FRAME_DURATION,walkLoopTextures,
+        val walkLoopTextures: com.badlogic.gdx.utils.Array<TextureRegion> =
+                com.badlogic.gdx.utils.Array()
+        walkLoopTextures.add(TextureRegion(Texture("walk-1-right.png")))
+        walkLoopTextures.add(TextureRegion(Texture("walk-2-right.png")))
+        walkLoopTextures.add(TextureRegion(Texture("walk-3-right.png")))
+
+        mWalkLoop = Animation<TextureRegion>(WALK_LOOP_FRAME_DURATION, walkLoopTextures,
                 Animation.PlayMode.LOOP_PINGPONG)
-        val explosionTextures=Array(3,{i->{
-            TextureRegion(Texture("explosion-$i.png"))
-        }})
-        mExplosion=Animation(EXPLOSION_FRAME_DURATION,explosionTextures,Animation.PlayMode.NORMAL)
-        mExplosions=DelayedRemovalArray()
+
+        val explosionTextures: Array<TextureRegion> = Array()
+        explosionTextures.add(TextureRegion(Texture("explosion-1.png")))
+        explosionTextures.add(TextureRegion(Texture("explosion-2.png")))
+        explosionTextures.add(TextureRegion(Texture("explosion-3.png")))
+
+        mExplosion = Animation(EXPLOSION_FRAME_DURATION, explosionTextures,
+                Animation.PlayMode.NORMAL)
+        mExplosions = DelayedRemovalArray()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        mViewport.update(width, height, true)
+    }
+
+
+    override fun render() {
+        //移除或者新增爆炸动画
+        updateExplosions()
+        mViewport.apply()
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        mBatch.projectionMatrix=mViewport.camera.combined
+        mBatch.begin()
+        
+    }
+
+
+    private fun updateExplosions() {
+        mExplosions.begin()
+        for ((index, e) in mExplosions.withIndex()) {
+            if (e.isAnimationFinished()) {
+                mExplosions.removeIndex(index)
+            }
+        }
+        mExplosions.end()
+
+        if(MathUtils.random() < Gdx.graphics.deltaTime* EXPLOSION_SPAWN_RATE){
+            val position=Vector2(MathUtils.random(mViewport.worldWidth),
+                    MathUtils.random(mViewport.worldHeight))
+            mExplosions.add(OneShotAnimation(mExplosion,position,TimeUtils.nanoTime()))
+        }
     }
 
 
 }
+
+
+
+
+
+
+
+
+
--- core/src/com/ggaier/gigagalk/animation/OneShotAnimation.kt
+++ core/src/com/ggaier/gigagalk/animation/OneShotAnimation.kt
@@ -1,11 +1,24 @@
 package com.ggaier.gigagalk.animation
 
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.TimeUtils
+
 /**
  * Created by ggaier at 11/04/2017 .
  * jwenbo52@gmail.com
  */
-class OneShotAnimation{
+class OneShotAnimation(val mAnimation: Animation<TextureRegion>, val mPosition:Vector2,
+                       val mStartTimeNanos:Long){
 
+    inline fun elapsedTime():Float=MathUtils.nanoToSec*(TimeUtils.nanoTime()-
+            mStartTimeNanos)
+
+    inline fun getFrame():TextureRegion=mAnimation.getKeyFrame(elapsedTime())
+
+    inline fun isAnimationFinished():Boolean=mAnimation.isAnimationFinished(elapsedTime())
 
 
 }
