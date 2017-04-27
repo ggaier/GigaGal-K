b7f8af4d1a52866c3fd02a34b9792a8b1c712e17
ggaier <jwenbo52@gmail.com>
2017-04-27 23:06:45 +0800

    update kotlin version, and add debug platforms

--- core/src/com/ggaier/gigagalk/gigagal/Level.kt
+++ core/src/com/ggaier/gigagalk/gigagal/Level.kt
@@ -18,6 +18,14 @@ class Level {
 
     init {
         mPlatforms.add(Platform(70f, 30f, 20f, 20f))
+        addDebugPlatforms()
+    }
+
+    private fun addDebugPlatforms() {
+        mPlatforms.add(Platform(15f, 100f, 30f, 20f))
+        mPlatforms.add(Platform(75f, 90f, 100f, 65f))
+        mPlatforms.add(Platform(35f, 55f, 50f, 20f))
+        mPlatforms.add(Platform(10f, 20f, 20f, 9f))
     }
 
     fun update(delta: Float) {
