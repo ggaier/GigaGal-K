e54462ea1663ca23fa9e0ed3bc352be3735ac59e
ggaier <jwenbo52@gmail.com>
2017-05-04 00:16:46 +0800

    finish detecting the collision of circles and rectangles

--- core/src/com/ggaier/gigagalk/rectanglecirclecollision/RectangleCircleCollisionScreen.kt
+++ core/src/com/ggaier/gigagalk/rectanglecirclecollision/RectangleCircleCollisionScreen.kt
@@ -15,63 +15,86 @@ import com.badlogic.gdx.utils.viewport.FitViewport
  * Created by ggaier
  * jwenbo52@gmail.com
  */
-class RectangleCircleCollisionScreen :ScreenAdapter(){
+class RectangleCircleCollisionScreen : ScreenAdapter() {
 
-    private val WORLD_SIZE =100f
-    private val CIRCLE_SEGMENT =64
+    private val WORLD_SIZE = 100f
+    private val CIRCLE_SEGMENT = 64
 
-    private val mStartTime=TimeUtils.nanoTime()
-    private val mShapeRenderer:ShapeRenderer= ShapeRenderer()
-    private val mViewport:FitViewport= FitViewport(WORLD_SIZE,WORLD_SIZE)
-    private val mRectangles:Array<Rectangle> = Array()
-    private val mCircles:Array<OscillatingCircle> = Array()
+    private val mStartTime = TimeUtils.nanoTime()
+    private val mShapeRenderer: ShapeRenderer = ShapeRenderer()
+    private val mViewport: FitViewport = FitViewport(WORLD_SIZE, WORLD_SIZE)
+    private val mRectangles: Array<Rectangle> = Array()
+    private val mCircles: Array<OscillatingCircle> = Array()
 
-    init{
-        mRectangles.add(Rectangle(40f,40f,20f,20f))
-        mRectangles.add(Rectangle(10f,40f,10f,20f))
-        mRectangles.add(Rectangle(70f,45f,20f,10f))
+    init {
+        mRectangles.add(Rectangle(40f, 40f, 20f, 20f))
+        mRectangles.add(Rectangle(10f, 40f, 10f, 20f))
+        mRectangles.add(Rectangle(70f, 45f, 20f, 10f))
 
-        mCircles.add(OscillatingCircle(50f,65f,7f,0f,40f,3f))
-        mCircles.add(OscillatingCircle(50f,35f,7f,0f,40f,3.1f))
-        mCircles.add(OscillatingCircle(50f,50f,3f,MathUtils.PI/4,40f,5f))
-        mCircles.add(OscillatingCircle(25f,50f,5f,0f,11f,7f))
+        mCircles.add(OscillatingCircle(50f, 65f, 7f, 0f, 40f, 3f))
+        mCircles.add(OscillatingCircle(50f, 35f, 7f, 0f, 40f, 3.1f))
+        mCircles.add(OscillatingCircle(50f, 50f, 3f, MathUtils.PI / 4, 40f, 5f))
+        mCircles.add(OscillatingCircle(25f, 50f, 5f, 0f, 11f, 7f))
     }
 
 
     override fun resize(width: Int, height: Int) {
-        mViewport.update(width,height,true)
+        mViewport.update(width, height, true)
     }
 
     override fun render(delta: Float) {
-        val elapsedTime=MathUtils.nanoToSec*TimeUtils.timeSinceNanos(mStartTime)
+        val elapsedTime = MathUtils.nanoToSec * TimeUtils.timeSinceNanos(mStartTime)
         mViewport.apply()
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        mShapeRenderer.projectionMatrix=mViewport.camera.combined
+        mShapeRenderer.projectionMatrix = mViewport.camera.combined
         mShapeRenderer.begin(ShapeRenderer.ShapeType.Line)
-        mShapeRenderer.setColor(1f,1f,1f,1f)
+        mShapeRenderer.setColor(1f, 1f, 1f, 1f)
 
-        for(rectangle in mRectangles){
-            mShapeRenderer.rect(rectangle.x,rectangle.y,rectangle.width,
+        for (rectangle in mRectangles) {
+            mShapeRenderer.rect(rectangle.x, rectangle.y, rectangle.width,
                     rectangle.height)
         }
         mCircles
                 .map { it.getCurrentCircle(elapsedTime) }
-                .map { circle -> mRectangles.any { areColliding(it,circle) } }
+                .map { circle -> mRectangles.any { areColliding(it, circle) } }
                 .forEach {
-                    if(it){
-                        mShapeRenderer.setColor(1f,0f,0f,1f)
-                    }else{
-                        mShapeRenderer.setColor(0f,1f,0f,1f)
+                    if (it) {
+                        mShapeRenderer.setColor(1f, 0f, 0f, 1f)
+                    } else {
+                        mShapeRenderer.setColor(0f, 1f, 0f, 1f)
                     }
                 }
+
+        for(oscillatingCircle in mCircles){
+            val circle =oscillatingCircle.getCurrentCircle(elapsedTime)
+            val colliding= mRectangles.any { areColliding(it,circle) }
+            if (colliding) {
+                mShapeRenderer.setColor(1f, 0f, 0f, 1f)
+            } else {
+                mShapeRenderer.setColor(0f, 1f, 0f, 1f)
+            }
+            mShapeRenderer.circle(circle.x,circle.y,circle.radius,CIRCLE_SEGMENT)
+        }
 
         mShapeRenderer.end()
 
     }
 
-    private fun areColliding(rect: Rectangle?, circle: Circle): Boolean {
-        
+    private fun areColliding(rect: Rectangle, circle: Circle): Boolean {
+        val containsACorner = circle.contains(rect.x, rect.y) ||
+                circle.contains(rect.x + rect.width, rect.y) || circle.contains(rect.x,
+                rect.y + rect.height) || circle.contains(rect.x + rect.width, rect.y + rect.height)
+        val inHorizontalInterval = rect.x < circle.x && circle.x < rect.x + rect.width
+        val inVerticalInterval = rect.y < circle.y && circle.y < rect.y + rect.height
+        val inHorizontalNeighborhood = rect.x - circle.radius < circle.x &&
+                circle.x < rect.x + rect.width + circle.radius
+        val inVerticalNeighborhood = rect.y - circle.radius < circle.y
+                && circle.y < rect.y + rect.height + circle.radius
+
+        val touchingAnEdge = inHorizontalInterval && inVerticalNeighborhood ||
+                inVerticalInterval && inHorizontalNeighborhood
+        return touchingAnEdge || containsACorner
     }
 
 
--- desktop/src/com/ggaier/gigagalk/desktop/DesktopLauncher.kt
+++ desktop/src/com/ggaier/gigagalk/desktop/DesktopLauncher.kt
@@ -2,11 +2,12 @@ package com.ggaier.gigagalk.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import com.ggaier.gigagalk.gigagal.GigagalGame
+import com.ggaier.gigagalk.rectanglecirclecollision.RectangleCircleCollisionGame
 
 object DesktopLauncher {
     @JvmStatic fun main(arg: Array<String>) {
         val config = LwjglApplicationConfiguration()
-        LwjglApplication(GigagalGame(), config)
+//        LwjglApplication(GigagalGame(), config)
+        LwjglApplication(RectangleCircleCollisionGame(), config)
     }
 }
