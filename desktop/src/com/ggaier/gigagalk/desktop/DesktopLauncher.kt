c863bc3275f1e1edd587342be4dc6613c4094f7b
ggaier <jwenbo52@gmail.com>
2017-05-06 00:20:56 +0800

    select which game to start from command line

--- desktop/src/com/ggaier/gigagalk/desktop/DesktopLauncher.kt
+++ desktop/src/com/ggaier/gigagalk/desktop/DesktopLauncher.kt
@@ -1,13 +1,34 @@
 package com.ggaier.gigagalk.desktop
 
+import com.badlogic.gdx.ApplicationListener
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.ggaier.gigagalk.gigagal.GigagalGame
 import com.ggaier.gigagalk.rectanglecirclecollision.RectangleCircleCollisionGame
+import java.util.*
 
 object DesktopLauncher {
     @JvmStatic fun main(arg: Array<String>) {
-        val config = LwjglApplicationConfiguration()
-//        LwjglApplication(GigagalGame(), config)
-        LwjglApplication(RectangleCircleCollisionGame(), config)
+        var applicationListener: ApplicationListener?=null
+        while (true) {
+            println("Input which game you want start: " + Arrays.toString(Games.values()))
+            val input = readLine()?.trim()
+            println("input is $input")
+            when (input) {
+                Games.GIGAGAL.name -> applicationListener = GigagalGame()
+                Games.COLLISION.name -> applicationListener = RectangleCircleCollisionGame()
+                else -> {
+                    println("Cannot find the Game $input to start, try again. ")
+                }
+            }
+            if(applicationListener!=null)
+                break
+        }
+        LwjglApplication(applicationListener, LwjglApplicationConfiguration())
+    }
+
+    enum class Games {
+        GIGAGAL,
+        COLLISION
     }
 }
