82289d728061e7e22052b59122891cda2cec3254
ggaier <jwenbo52@gmail.com>
2017-05-15 13:01:50 +0800

    udpate android gradle plugin, finish levelloader

--- core/src/com/ggaier/gigagalk/gigagal/util/LevelLoader.kt
+++ core/src/com/ggaier/gigagalk/gigagal/util/LevelLoader.kt
@@ -22,28 +22,30 @@ class LevelLoader {
         private val TAG = "LevelLoader"
 
         @JvmStatic
-        fun load(levelName: String, viewport: Viewport) {
+        fun load(levelName: String, viewport: Viewport): Level {
             val level = Level(viewport)
             val path = "levels${File.separator}$levelName.json"
             Gdx.app.log(TAG, "path $path")
             val file = Gdx.files.internal(path)
             try {
                 val parser = JSONParser()
-                val rootJsonObject:JSONObject = parser.parse(file.reader()) as JSONObject
-                Gdx.app.log(TAG,rootJsonObject.keys.toString())
+                val rootJsonObject: JSONObject = parser.parse(file.reader()) as JSONObject
+                Gdx.app.log(TAG, rootJsonObject.keys.toString())
 
-                val composite:JSONObject = rootJsonObject[LEVEL_COMPOSITE] as JSONObject
-                Gdx.app.log(TAG,composite.keys.toString())
+                val composite: JSONObject = rootJsonObject[LEVEL_COMPOSITE] as JSONObject
+                Gdx.app.log(TAG, composite.keys.toString())
 
-                val platforms:JSONArray = composite[LEVEL_9PATCHES] as JSONArray
-                val firstPlatform:JSONObject=platforms[0] as JSONObject
-                Gdx.app.log(TAG,firstPlatform.keys.toString())
+                val platforms: JSONArray = composite[LEVEL_9PATCHES] as JSONArray
+                val firstPlatform: JSONObject = platforms[0] as JSONObject
+                Gdx.app.log(TAG, firstPlatform.keys.toString())
             } catch(e: Exception) {
-                Gdx.app.error(TAG,e.message)
+                Gdx.app.error(TAG, e.message)
                 Gdx.app.error(TAG, LEVEL_ERROR_MESSAGE)
             }
+            return level
         }
 
+        
     }
 
 }
